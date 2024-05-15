c5820dc1db332a01e2fd98cefa77ff91666c7401
LaraAcuna <lara.acuna@est.fi.uncoma.edu.ar>
2024-05-15 20:35:56 -0300

    tests de getters, transiciones de estado

--- src/test/java/com/acunapuchetavv/PlantacionTests.java
+++ src/test/java/com/acunapuchetavv/PlantacionTests.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Assertions;
 
 public class PlantacionTests {
     @Test
-    public void calcularPo(){
+                public void calcularPo(){
         Plantacion test = new Plantacion();
         double po = test.calcularPO(0, 8000);
         Assertions.assertEquals(100, po);
@@ -47,5 +47,70 @@ public class PlantacionTests {
         int estado = test.calcularEstado(-5);
         Assertions.assertEquals(2, estado);
     }
+
+    @Test
+    @DisplayName("Testeando getParcelas")
+    public void testearGetParcelas(){
+        Plantacion test = new Plantacion();
+        int parcelas = test.getParcelas();
+        Assertions.assertEquals(2, parcelas);
+    }
+
+    @Test
+    @DisplayName("Transición Improductivo a Productivo - TRUE")
+    public void testearImproductivoAProductivo(){
+        Plantacion test = new Plantacion();
+        boolean transicionValida = test.transicionValida(Plantacion.ESTADO_IMPRODUCTIVO, Plantacion.ESTADO_PRODUCTIVO);
+        Assertions.assertTrue(transicionValida);
+    }
+
+    @Test
+    @DisplayName("Transición Productivo a ConReserva - TRUE")
+    public void testearProductivoAConReserva(){
+        Plantacion test = new Plantacion();
+        boolean transicionValida = test.transicionValida(Plantacion.ESTADO_PRODUCTIVO, Plantacion.ESTADO_CON_RESERVA);
+        Assertions.assertTrue(transicionValida);
+    }
+
+    @Test
+    @DisplayName("Transición ConReserva a Excedido - TRUE")
+    public void testearConReservaAExcedido(){
+        Plantacion test = new Plantacion();
+        boolean transicionValida = test.transicionValida(Plantacion.ESTADO_CON_RESERVA, Plantacion.ESTADO_EXCEDIDO);
+        Assertions.assertTrue(transicionValida);
+    }
+
+    @Test
+    @DisplayName("Transición Excedido a ConReserva - TRUE")
+    public void testearExcedidoAConReserva(){
+        Plantacion test = new Plantacion();
+        boolean transicionValida = test.transicionValida(Plantacion.ESTADO_EXCEDIDO, Plantacion.ESTADO_CON_RESERVA);
+        Assertions.assertTrue(transicionValida);
+    }
+
+    @Test
+    @DisplayName("Transición Improductivo a Excedido (Inválido) - FALSE")
+    public void testearTransicionInvalida(){
+        Plantacion test = new Plantacion();
+        boolean transicionInvalida = test.transicionValida(Plantacion.ESTADO_IMPRODUCTIVO, Plantacion.ESTADO_EXCEDIDO);
+        Assertions.assertFalse(transicionInvalida);
+    }
+
+    @Test
+    @DisplayName("Testeando getCultivos")
+    public void testearGetCultivos(){
+        Plantacion test = new Plantacion();
+        int cultivos = test.getCultivos();
+        Assertions.assertEquals(0, cultivos);
+    }
+
+    @Test
+    @DisplayName("Testeando getMetrosOcupados")
+    public void testearGetMetrosOcupados(){
+        Plantacion test = new Plantacion();
+        double metrosOcupados = test.getMetrosOcupados();
+        Assertions.assertEquals(0, metrosOcupados);
+    }
+
 
 }
